8078f3bc0cc4dca27a09d6ced46735259b5f5f0f
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-24 10:33:44 +0700

    student management system

---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace studentsmanagementsystem
+{
+    class Student : IStudent
+    {
+
+        public string FullName { get; set; }
+        public int ID { get; set; }
+        public DateTime DateofBirth { get; set; }
+
+        public string Native { get; set; }
+        public string Class { get; set; }
+        public string PhoneNo { get; set; }
+        public int Mobile { get; set; }
+        public void Display()
+        {
+            Console.WriteLine($"{FullName}  {ID} {DateofBirth}  {Native}  {Class}  {PhoneNo}  {Mobile} ");
+        }
+
+    }
+}
